9729d9f80b0a614b012a8b97a9ae4573755ccf44
jtreeves <jackson.t.reeves@gmail.com>
2021-01-07 14:50:29 -0500

    [02-reverse] Reverse a string in two different ways

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,17 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+sentence = 'Look at the black dog jumped over the fence'
+print(sentence[::-1])
+
+num_array = [1,2,3]
+num_array.reverse()
+print(num_array)
+
+split_sentence = list(sentence)
+print(split_sentence)
+split_sentence.reverse()
+print(split_sentence)
+reverse_sentence = ''.join(map(str, split_sentence))
+print(reverse_sentence)
