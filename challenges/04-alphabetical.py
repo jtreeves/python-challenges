84ade64667308ceb527fa19fb2737837c2494cca
jtreeves <jackson.t.reeves@gmail.com>
2021-01-07 15:47:27 -0500

    [04-alphabetical] Alphabetise a string

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,10 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+word = 'poTatasdfEWSkkdhdTTUNEGSKuiebhwmdho'
+lower_word = word.lower()
+split_word = list(lower_word)
+split_word.sort()
+new_word = ''.join(map(str, split_word))
+print(new_word)
