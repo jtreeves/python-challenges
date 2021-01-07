2d489f0f642768aee8869569818cf3386b7ec6d4
jtreeves <jackson.t.reeves@gmail.com>
2021-01-07 15:32:18 -0500

    [03-bank] Build out display, deposit, and withdraw functions

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,35 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+print('Welcome to Chase bank.')
+balance = 1000
 
+def choose_action():
+    option = input('What would you like to do? (display, deposit, withdraw) ')
+    if option == 'display':
+        view_balance()
+    elif option == 'deposit':
+        deposit_money()
+    elif option == 'withdraw':
+        withdraw_money()
+    else:
+        print('Have a nice day!')
+
+def view_balance():
+    print('Your balance is: ${}'.format(balance))
+    choose_action()
+
+def deposit_money():
+    money = input('How much money would you like to deposit? ')
+    print('You have deposited ${} into your account.'.format(money))
+    money_int = int(money)
+    balance += money_int
+    view_balance()
+
+def withdraw_money():
+    money = input('How much money would you like to withrdaw? ')
+    print('You have withdrawn ${} from your account'.format(money))
+    money_int = int(money)
+    balance -= money_int
+    view_balance()
+
+choose_action()
+
+# print('Have a nice day!')
