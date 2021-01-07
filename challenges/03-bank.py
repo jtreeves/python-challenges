a712b5ea63fe79eaae53b683b87a4c019f17d16f
jtreeves <jackson.t.reeves@gmail.com>
2021-01-07 15:48:32 -0500

    [03-bank] Replace my code with Paul's

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,35 +1,24 @@
-print('Welcome to Chase bank.')
-balance = 1000
-
-def choose_action():
-    option = input('What would you like to do? (display, deposit, withdraw) ')
-    if option == 'display':
-        view_balance()
-    elif option == 'deposit':
-        deposit_money()
-    elif option == 'withdraw':
-        withdraw_money()
+def withdraw_money(account_balance):
+    amount = input('How much would you like to withdraw today?\n')
+    account_balance -= int(amount)
+    view_account_balance('Y',account_balance)
+def deposit_money(account_balance):
+    amount = input('How much would you like to deposit today?\n')
+    account_balance += int(amount)
+    view_account_balance('Y',account_balance)
+def view_account_balance(choice,account_balance):
+    if (choice == 'Y'):
+        print(f'Your account balance is ${account_balance}')
     else:
-        print('Have a nice day!')
-
-def view_balance():
-    print('Your balance is: ${}'.format(balance))
-    choose_action()
-
-def deposit_money():
-    money = input('How much money would you like to deposit? ')
-    print('You have deposited ${} into your account.'.format(money))
-    money_int = int(money)
-    balance += money_int
-    view_balance()
-
-def withdraw_money():
-    money = input('How much money would you like to withrdaw? ')
-    print('You have withdrawn ${} from your account'.format(money))
-    money_int = int(money)
-    balance -= money_int
-    view_balance()
-
-choose_action()
-
-# print('Have a nice day!')
+        add_or_take = input('Would you like to withdraw or deposit today?\n')
+        if (add_or_take == 'deposit'):
+            deposit_money(account_balance)
+        elif (add_or_take == 'withdraw'):
+            withdraw_money(account_balance)
+        else:
+            view_account_balance('Y',account_balance)
+print("Welcome to Chase bank.")
+account_balance = 5000
+choice = input('Would you like to view your account balance? Y/N\n')
+view_account_balance(choice,account_balance)
+print("Have a nice day!")
