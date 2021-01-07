65b280f735b6404be859e74785e8277d4d152735
jtreeves <jackson.t.reeves@gmail.com>
2021-01-07 14:26:48 -0500

    [01-calc] Take input, manipulate it, and provide different output

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,8 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+age_string = input('How old are you? ')
+age_int = int(age_string)
+age_plus_one = age_int + 1
+print('In one year, you will be {} years old.'.format(age_plus_one))
